418f8bfaea31349be96de0283b72171b1689d2b9
GavinTseYinWang <171096@trinityschools.nz>
2019-08-13 10:14:59 +1200

    etghrtg

--- 01_Variable/Module1.vb
+++ 01_Variable/Module1.vb
@@ -5,6 +5,17 @@
 
         Dim Var14b13 As String = "Weird var name"
 
+        Dim StudentAge As Integer = 16
+        Dim intStudenAge As Integer = 17
+
+        Console.WriteLine(StudentAge)
+        Console.WriteLine(intStudenAge)
+        Console.WriteLine(MYMESSAGE)
+
+        Console.WriteLine(Var14b13)
+
+        Console.ReadLine()
+
     End Sub
 
 
